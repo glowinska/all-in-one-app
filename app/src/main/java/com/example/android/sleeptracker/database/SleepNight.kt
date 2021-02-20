9061eaf957c7a184b36e6ced4d29a0ff2f7ef57a
glowinska <anna.glowinska98@gmail.com>
2021-02-20 23:49:40 +0100

    sleep night entity

--- app/src/main/java/com/example/android/sleeptracker/database/SleepNight.kt
+++ app/src/main/java/com/example/android/sleeptracker/database/SleepNight.kt
@@ -16,3 +16,21 @@
 
 package com.example.android.sleeptracker.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+        @PrimaryKey(autoGenerate = true)
+        var nightId: Long = 0L,
+
+        @ColumnInfo(name = "start_time_ms")
+        val startTimeMs: Long = System.currentTimeMillis(),
+
+        @ColumnInfo(name = "end_time_ms")
+        var endTimeMs: Long = startTimeMs,
+
+        @ColumnInfo(name = "")
+        var sleepQuality: Int = -1
+)
