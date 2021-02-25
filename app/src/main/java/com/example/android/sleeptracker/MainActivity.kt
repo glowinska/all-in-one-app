cf3d29c5039d68f53489d2482d99758577de0455
glowinska <anna.glowinska98@gmail.com>
2021-02-25 18:14:28 +0100

    toolbar & icons

--- app/src/main/java/com/example/android/sleeptracker/MainActivity.kt
+++ app/src/main/java/com/example/android/sleeptracker/MainActivity.kt
@@ -35,10 +35,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val toolbar: Toolbar = findViewById(R.id.toolbar)
-//        setSupportActionBar(toolbar)
-
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.setScrimColor(getResources().getColor(android.R.color.transparent));
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
