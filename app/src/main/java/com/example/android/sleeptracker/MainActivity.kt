e182dadb51f3f318065e0fe7711b231c4c8378dc
glowinska <anna.glowinska98@gmail.com>
2021-03-03 14:13:02 +0100

    mars real estate

--- app/src/main/java/com/example/android/sleeptracker/MainActivity.kt
+++ app/src/main/java/com/example/android/sleeptracker/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.sleep_tracker_fragment), drawerLayout)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.sleep_tracker_fragment, R.id.mars_fragment), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
