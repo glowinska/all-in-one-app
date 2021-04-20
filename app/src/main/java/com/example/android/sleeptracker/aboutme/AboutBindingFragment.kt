2693e961fdd9dec3db4f5029a6c363e78e94f515
glowinska <anna.glowinska98@gmail.com>
2021-04-20 11:32:48 +0200

    kotlinize binding

--- app/src/main/java/com/example/android/sleeptracker/aboutme/AboutBindingFragment.kt
+++ app/src/main/java/com/example/android/sleeptracker/aboutme/AboutBindingFragment.kt
@@ -6,18 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import com.example.android.sleeptracker.R
 import com.example.android.sleeptracker.databinding.FragmentAboutBindingBinding
 import kotlinx.android.synthetic.main.fragment_about_me.*
 
 
 class AboutBindingFragment : Fragment() {
 
-    // private lateinit var aboutBindingViewModel: AboutMeViewModel
+    private lateinit var aboutBindingViewModel: AboutMeViewModel
     private var _binding: FragmentAboutBindingBinding? = null
     private val binding get() = _binding!!
 
@@ -29,33 +26,38 @@ class AboutBindingFragment : Fragment() {
     ): View? {
         _binding = FragmentAboutBindingBinding.inflate(inflater, container, false)
         val view = binding.root
-//        aboutBindingViewModel =
-//                ViewModelProviders.of(this).get(AboutMeViewModel::class.java)
-//        val root = inflater.inflate(R.layout.fragment_about_binding, container, false)
-        binding.doneButton.setOnClickListener {
-            addNickname(it)
-        }
-        binding.nicknameText.setOnClickListener {
-            updateNickname(it)
+        aboutBindingViewModel =
+                ViewModelProviders.of(this).get(AboutMeViewModel::class.java)
+        binding.apply {
+            doneButton.setOnClickListener {
+                addNickname()
+            }
+            nicknameText.setOnClickListener {
+                updateNickname()
+            }
         }
         return view
     }
 
-    private fun addNickname(viewThatIsClicked: View) {
-        binding.nicknameText.text = binding.nicknameEdit.text
-        binding.nicknameText.visibility = View.VISIBLE
-        viewThatIsClicked.visibility = View.GONE
-        binding.nicknameEdit.visibility = View.GONE
+    private fun addNickname() {
+        binding.apply {
+            nicknameText.text = nicknameEdit.text
+            nicknameText.visibility = View.VISIBLE
+            doneButton.visibility = View.GONE
+            nicknameEdit.visibility = View.GONE
+        }
         // Hide the keyboard.
         val inputMethodManager = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(requireView().windowToken, 0)
     }
 
-    private fun updateNickname(view: View) {
-        binding.nicknameEdit.visibility = View.VISIBLE
-        binding.doneButton.visibility = View.VISIBLE
-        view.visibility = View.GONE
-        binding.nicknameEdit.requestFocus()
+    private fun updateNickname() {
+        binding.apply {
+            nicknameEdit.visibility = View.VISIBLE
+            doneButton.visibility = View.VISIBLE
+            nicknameText.visibility = View.GONE
+            nicknameEdit.requestFocus()
+        }
         // Show the keyboard.
         val inputMethodManager = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.showSoftInput(nickname_edit, 0)
---
+++ app/src/main/java/com/example/android/sleeptracker/aboutme/MyName.kt
@@ -0,0 +1,4 @@
+package com.example.android.sleeptracker.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "") {
+}
