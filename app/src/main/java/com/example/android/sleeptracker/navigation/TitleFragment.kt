f748fb8f696fa56806d9ea87ab28b2689e74529c
glowinska <anna.glowinska98@gmail.com>
2021-04-23 00:02:43 +0200

    navigation for a game

--- app/src/main/java/com/example/android/sleeptracker/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/sleeptracker/navigation/TitleFragment.kt
@@ -6,8 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.android.sleeptracker.R
+import com.example.android.sleeptracker.database.SleepDatabase
+import com.example.android.sleeptracker.databinding.FragmentSleepTrackerBinding
 import com.example.android.sleeptracker.databinding.FragmentTitleBinding
+import com.example.android.sleeptracker.overview.OverviewFragmentDirections
+import com.example.android.sleeptracker.sleepquality.SleepQualityFragmentDirections
+import com.example.android.sleeptracker.sleepquality.SleepQualityViewModel
+import com.example.android.sleeptracker.sleepquality.SleepQualityViewModelFactory
+import com.example.android.sleeptracker.sleeptracker.SleepTrackerFragmentDirections
+import com.example.android.sleeptracker.sleeptracker.SleepTrackerViewModel
+import com.example.android.sleeptracker.sleeptracker.SleepTrackerViewModelFactory
 
 class TitleFragment : Fragment() {
 
@@ -15,6 +27,10 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,
                 R.layout.fragment_title, container,false)
+
+        binding.playButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
 }
